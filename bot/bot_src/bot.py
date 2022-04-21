aef9efe751a218fa3f7ae1fdc0a4d548f4128476
Kdavovadk <102761176+Kdavovadk@users.noreply.github.com>
2022-04-21 20:02:22 +0300

    Update bot.py

---
+++ bot/bot_src/bot.py
@@ -0,0 +1,24 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from config import TOKEN
+
+
+def start(update, context):
+    btn1 = InlineKeyboardButton('1', callback_data="1st_button")
+    btn2 = InlineKeyboardButton('2', callback_data="2nd_button")
+    rm = InlineKeyboardMarkup([[btn1, btn2]])
+    update.message.reply_text('Привет!', reply_markup=rm)
+
+
+def echo(update, context):
+    update.message.reply_text('Сообщение: "' + update.message.text + '"')
+
+
+botUpdater = Updater(TOKEN)
+
+botDispatcher = botUpdater.dispatcher
+botDispatcher.add_handler(CommandHandler("start", start))
+botDispatcher.add_handler(MessageHandler(Filters.text, echo))
+
+botUpdater.start_polling()
+botUpdater.idle()
