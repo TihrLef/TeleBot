693a55061d96c4c537afd6bc8c0571c1a281649f
Kdavovadk <kosnevich.1999@gmail.com>
2022-04-22 10:39:33 +0300

    Update bot.py

--- bot/bot_src/bot.py
+++ bot/bot_src/bot.py
@@ -1,25 +1,54 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from config import TOKEN
 
 
-def start(update, context):
-    btn1 = InlineKeyboardButton('1', callback_data="1st_button")
-    btn2 = InlineKeyboardButton('2', callback_data="2nd_button")
-    rm = InlineKeyboardMarkup([[btn1, btn2]])
-    update.message.reply_text("Привет!"
-                              "Этот бот предназначен для управления активными проектами.", reply_markup=rm)
+def start(update, _):
+    user = update.message.from_user
+    update.message.reply_text("Привет, " + user.first_name +
+                              "\nЭтот бот предназначен для управления существующими проектами."
+                              "\nОтправьте /addreport для добавления отчета.")
 
 
-def echo(update, context):
+def projectSelect(update, _):
+    inlineButtons = [
+        [InlineKeyboardButton("Создание бота", callback_data="bot")],
+        [InlineKeyboardButton("Создание веб-интерфейса", callback_data="web")]
+    ]
+    inlineMarkup = InlineKeyboardMarkup(inlineButtons)
+
+    update.message.reply_text("Выберите проект:", reply_markup=inlineMarkup)
+
+
+def botProject(update, callback):
+    query = update.callback_query
+    query.answer()
+
+    query.edit_message_text('Выбран проект "Создание бота"')
+
+    inlineButtons = [
+        [InlineKeyboardButton("11.04.2022-16.04.2022", callback_data="first")]
+    ]
+    inlineMarkup = InlineKeyboardMarkup(inlineButtons)
+
+    callback.bot.send_message(update.effective_chat.id, "Выберите дату:", reply_markup=inlineMarkup)
+
+
+def echo(update, _):
     update.message.reply_text('Сообщение: "' + update.message.text + '"')
 
 
-botUpdater = Updater(TOKEN)
+def main():
+    botUpdater = Updater(TOKEN)
 
-botDispatcher = botUpdater.dispatcher
-botDispatcher.add_handler(CommandHandler("start", start))
-botDispatcher.add_handler(MessageHandler(Filters.text, echo))
+    botDispatcher = botUpdater.dispatcher
+    botDispatcher.add_handler(CommandHandler("start", start))
+    botDispatcher.add_handler(CommandHandler("addreport", projectSelect))
+    botDispatcher.add_handler(CallbackQueryHandler(botProject, pattern='^' + "bot" + '$'))
 
-botUpdater.start_polling()
-botUpdater.idle()
+    botUpdater.start_polling()
+    botUpdater.idle()
+
+
+if __name__ == "__main__":
+    main()
