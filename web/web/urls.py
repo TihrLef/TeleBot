4d41d470b7383812144c2b2aad2a2a337664748c
ai1791 <ai.tikhonova@mail.ru>
2022-04-22 19:23:46 +0300

    for

--- web/web/urls.py
+++ web/web/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from Projects import views
+from Users.views import SignUpView
 urlpatterns = []
 
 urlpatterns += [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("admin/", admin.site.urls),
+    path("sign_up/", include ("Users.urls"), name = "signup"),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
 
