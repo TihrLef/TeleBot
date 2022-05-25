28b53dc44b3b3a6745b86c6b6d616493129655a1
Gigerin <geidar.mamedov1@gmail.com>
2022-05-25 18:58:33 +0300

    Fixing pdf(trying)

--- web/TeleBot/views.py
+++ web/TeleBot/views.py
@@ -16,7 +16,8 @@ import tempfile
 from tempfile import TemporaryDirectory as td
 from django.contrib.auth.mixins import AccessMixin
 from threading import Thread
-import time
+import time, os
+from django.conf import settings
 from django.contrib.admin.views.decorators import staff_member_required
 
 
@@ -30,7 +31,7 @@ class OwnerOnlyMixin(AccessMixin):
         if request.user.telegram_id != user_page.telegram_id and not request.user.is_staff :
             return self.handle_no_permission()
         return super().dispatch(request, pk, *args, **kwargs)
-	
+
 
 
 import web.urls
@@ -64,7 +65,7 @@ def index(request):
 def available_reports(request):
 	if(request.user.is_staff):
 		return Report.objects.all()
-	return Report.objects.filter(Q(user__exact = request.user.telegram_id) | 
+	return Report.objects.filter(Q(user__exact = request.user.telegram_id) |
 							  Q(project__responsible_user__telegram_id__exact = request.user.telegram_id)).order_by('project')
 
 def available_projects(request):
@@ -82,13 +83,11 @@ def available_users(request):
 	return QuerySet(set(users))
 
 def make_pdf(request, reports, path):
-	temp = TempDir()
-	temp.MakeDir(path = path, prefix = 'TempPdf', lifetime = 180)
-	name = temp.name
+	name = r"~/TeleBot/web/TeleBot/static/pdf_folder"
 	pdf = FPDF()
 	pdf.add_page()
-	pdf.add_font("Sans", style = "", fname = r"TeleBot/static/Fonts/OpenSans/OpenSans-Regular.ttf", uni=True)
-	pdf.add_font("Sans", style = "B", fname = r"TeleBot/static/Fonts/OpenSans/OpenSans-Bold.ttf", uni=True)
+	pdf.add_font("Sans", style = "", fname = os.path.join(settings.BASE_DIR, "TeleBot/static/Fonts/OpenSans/OpenSans-Regular.ttf"), uni=True)
+	pdf.add_font("Sans", style = "B", fname = os.path.join(settings.BASE_DIR, "TeleBot/static/Fonts/OpenSans/OpenSans-Bold.ttf"), uni=True)
 	for report in reports:
 		pdf.set_font("Sans", style = "B", size = 12)
 		pdf.multi_cell(w = 200, h = 8, txt = 'Project name: ' + report.project.name, align = "L", ln = 1)
@@ -98,22 +97,22 @@ def make_pdf(request, reports, path):
 		pdf.set_font("Sans", style = "", size = 12)
 		pdf.multi_cell(w = 200, h = 8, txt = report.message, align = "L", ln = 1)
 		pdf.multi_cell(w = 200, h = 10, txt = '\n', align = "L", ln = 1)
-	pdf.output(name + r"/simple_demo" + str(request.user) + ".pdf", "F")
+		pdf.output(name +  r"/simple_demo" + str(request.user) + ".pdf", path)
 	return name
 
 @user_passes_test(User.is_verified)
 def report(request):
 	reports = available_reports(request)
 	error_message = ''
-	
+
 	form = FilterForm()
 	if request.method == 'POST':
 		form = FilterForm(request.POST)
 		if(form.is_valid()):
 			data = form.cleaned_data
-			if(data['project']): 
+			if(data['project']):
 				reports = reports.filter(project__id__in = data['project'])
-			if(data['user']): 
+			if(data['user']):
 				reports = reports.filter(user__telegram_id__in = data['user'])
 			if(data['left_date']):
 				reports = reports.exclude(report_date__lte = data['left_date'])
@@ -123,9 +122,9 @@ def report(request):
 			error_message = 'incorrect input data'
 			reports = []
 
-	path = r"TeleBot/static"
+	path = r"~/TeleBot/web/TeleBot/static/pdf_folder"
 	name = make_pdf(request, reports, path)
-	
+
 	projects = available_projects(request)
 	users = available_users(request)
 	if len(users) == 1:
@@ -142,7 +141,7 @@ def report(request):
 			 'error_message': error_message,
 			 'form': form,
 			 'pdfname': name[len(path):] + r"/simple_demo" + str(request.user) + ".pdf"})
-	
+
 
 class UsersListView(generic.ListView):
 	model = User
@@ -150,13 +149,13 @@ class UsersListView(generic.ListView):
 class UserDetailView(OwnerOnlyMixin, generic.DetailView):
 	model = User
 
-@user_passes_test(User.is_verified)	
+@user_passes_test(User.is_verified)
 def user_detail(request,pk):
 	try:
 		tele_id=User.objects.get(telegram_id=pk)
 	except User.DoesNotExist:
 		raise Http404("Такого персонажа не существует!")
-	
+
 	return render(
 		request,
 		'user/user_detail.html',
@@ -189,11 +188,11 @@ def send_contact(request):
      subject = request.POST.get("subject")
      message = request.POST.get("message")
      send_mail("Новое сообщение", message, email, ["codenamedelta91@gmail.com"],
-    html_message="<html> Новое сообщение с сайта"
-      "Имя:" + name + '\n'
-      "Email почта:" + email + '\n'
-      "Тема:" + subject + '\n'
-       "Сообщение:" + message + "\n"
+    html_message="<html> Новое сообщение с сайта<br>"
+      "Имя:" + name + '<br>'
+      "Email почта:" + email + '<br>'
+      "Тема:" + subject + '<br>'
+       "Сообщение:" + message + "<br>"
    "</html>")
      request.session['sendmessage'] = "Сообщение было отправлено"
      return HttpResponseRedirect('contact-page')
