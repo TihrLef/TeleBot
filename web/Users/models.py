42f1460c27acb4b54ceb88e7abdf70e7733219b0
Timofey Bobylkov <bobylkovt@gmail.com>
2022-05-25 17:29:31 +0300

    make_from_dict [telegram JSON] method added
    Signed-off-by: Timofey Bobylkov <bobylkovt@gmail.com>

--- web/Users/models.py
+++ web/Users/models.py
@@ -4,35 +4,47 @@ import uuid
 # Create your models here.
 
 from django.db import models
-from django.urls import reverse 
+from django.urls import reverse
 from datetime import date
 from django.urls import reverse
-	
+
 
 class User(AbstractUser):
-	personal_token = models.UUIDField(default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
-	telegram_id = models.BigIntegerField(primary_key=True, unique=True)
-	def __str__(self):
-		"""String for representing the Model object"""
-		return self.username
-	def get_absolute_url(self):
-		return reverse('user-detail', args=[str(self.telegram_id)])
-    
-	def my_view(self, request):
-		username = None
-		if request.user.is_authenticated():
-			username = request.user.username
+    personal_token = models.UUIDField(default=uuid.uuid4,
+                                      help_text="Unique ID for this particular book across whole library")
+    telegram_id = models.BigIntegerField(primary_key=True, unique=True)
 
-	@property
-	def get_role(self):
-		if self.is_staff:
-			return "Администратор"
-		else:
-			if self.is_active:
-				return "Подтвержденный"
-			else:
-				return "Неподтвержденный"
-	
-	def is_verified(self):
-		return (self.is_active) or (self.is_staff)
-	
+    def __str__(self):
+        """String for representing the Model object"""
+        return self.username
+
+    def get_absolute_url(self):
+        return reverse('user-detail', args=[str(self.telegram_id)])
+
+    def my_view(self, request):
+        username = None
+        if request.user.is_authenticated():
+            username = request.user.username
+
+    @property
+    def get_role(self):
+        if self.is_staff:
+            return "Администратор"
+        else:
+            if self.is_active:
+                return "Подтвержденный"
+            else:
+                return "Неподтвержденный"
+
+    def is_verified(self):
+        return self.is_active or self.is_staff
+
+    @staticmethod
+    def make_from_dict(data):
+        user, created = User.objects.get_or_create(telegram_id=data.get('id', ''))
+        user.first_name = data.get('first_name', '')
+        user.last_name = data.get('last_name', '')
+        user.username = data.get('username', '')
+        user.is_active = True
+
+        return user
