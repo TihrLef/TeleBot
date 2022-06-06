036f23bc703ab6d78511eb11b7b0c6cbcc4688b2
Gigerin <geidar.mamedov1@gmail.com>
2022-06-06 15:25:50 +0300

    import restructuration from main

--- web/Reports/views.py
+++ web/Reports/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.contrib.auth.decorators import user_passes_test
@@ -13,26 +14,10 @@ import time
 from django.db.models import Q
 from web import settings
 
-class TempDir:
-	def __init__(self):
-		self.name = None
-	def MakeDir(self, path = None, prefix = '', lifetime = 0):
-		th = Thread(target = self.creation, args=(path, prefix, lifetime))
-		th.start()
-		while self.name is None:
-			time.sleep(0.01)
-
-	def creation(self, path = None, prefix = '', lifetime = 0):
-		if path:
-			tempfile.tempdir = path
-		temp = td(prefix = prefix)
-		self.name = str(temp.name)
-		time.sleep(lifetime)
-
 def available_reports(request):
 	if(request.user.is_staff):
 		return Report.objects.all()
-	return Report.objects.filter(Q(user__exact = request.user.telegram_id) | 
+	return Report.objects.filter(Q(user__exact = request.user.telegram_id) |
 							  Q(project__responsible_user__telegram_id__exact = request.user.telegram_id)).order_by('project')
 
 def available_projects(request):
@@ -50,13 +35,11 @@ def available_users(request):
 	return users
 
 def make_pdf(request, reports, path):
-	temp = TempDir()
-	temp.MakeDir(path = path, prefix = 'TempPdf', lifetime = 180)
-	name = temp.name
+	os.makedirs(path, exist_ok = True)
 	pdf = FPDF()
 	pdf.add_page()
-	pdf.add_font("Sans", style = "", fname = r"Reports/static/Fonts/OpenSans/OpenSans-Regular.ttf", uni=True)
-	pdf.add_font("Sans", style = "B", fname = r"Reports/static/Fonts/OpenSans/OpenSans-Bold.ttf", uni=True)
+	pdf.add_font("Sans", style = "", fname = os.path.join(settings.BASE_DIR, "Reports/static/Fonts/OpenSans/OpenSans-Regular.ttf"), uni=True)
+	pdf.add_font("Sans", style = "B", fname = os.path.join(settings.BASE_DIR, "Reports/static/Fonts/OpenSans/OpenSans-Bold.ttf"), uni=True)
 	for report in reports:
 		pdf.set_font("Sans", style = "B", size = 12)
 		pdf.multi_cell(w = 200, h = 8, txt = 'Project name: ' + report.project.name, align = "L", ln = 1)
@@ -66,22 +49,21 @@ def make_pdf(request, reports, path):
 		pdf.set_font("Sans", style = "", size = 12)
 		pdf.multi_cell(w = 200, h = 8, txt = report.message, align = "L", ln = 1)
 		pdf.multi_cell(w = 200, h = 10, txt = '\n', align = "L", ln = 1)
-	pdf.output(name + r"/simple_demo" + str(request.user) + ".pdf", "F")
-	return name
+	pdf.output(path + r"/" + str(request.user) + ".pdf", "F")
 
 @user_passes_test(User.is_verified)
 def report(request):
 	reports = available_reports(request)
 	error_message = ''
-	
+
 	form = FilterForm()
 	if request.method == 'POST':
 		form = FilterForm(request.POST)
 		if(form.is_valid()):
 			data = form.cleaned_data
-			if(data['project']): 
+			if(data['project']):
 				reports = reports.filter(project__id__in = data['project'])
-			if(data['user']): 
+			if(data['user']):
 				reports = reports.filter(user__telegram_id__in = data['user'])
 			if(data['left_date']):
 				reports = reports.exclude(report_date__lte = data['left_date'])
@@ -92,14 +74,15 @@ def report(request):
 			reports = []
 
 	path = r"Reports/static/pdf_folder"
-	name = make_pdf(request, reports, path)
-	
+	make_pdf(request, reports, path)
+
 	projects = available_projects(request)
 	users = available_users(request)
 	if len(users) == 1:
 		users.clear()
 	if len(projects) == 1:
 		projects.clear()
+	print(form)
 	return render(
 		request,
 		'Reports/reports_list.html',
@@ -108,5 +91,4 @@ def report(request):
 			 'available_users': users,
 			 'error_message': error_message,
 			 'form': form,
-			 'pdfname': name[len(path):] + r"/simple_demo" + str(request.user) + ".pdf"})
-	
+			 'pdfname': r"pdf_folder/" + str(request.user) + ".pdf"})	
